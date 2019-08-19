d53ea0be8ef52d383318dee78534750dc8edaba9
georgemmp <machadogeorge.p@gmail.com>
2019-08-19 15:43:31 -0300

    cadastro de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
 const routes = express.Router()
+const UserController = require('./controller/user.controller')
+
+routes.post('/api/users', UserController.store)
 
 module.exports = routes
