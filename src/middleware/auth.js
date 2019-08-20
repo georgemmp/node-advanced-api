5313193deb04e6f83de5f6473b3a3b72044721fb
georgemmp <machadogeorge.p@gmail.com>
2019-08-20 14:31:29 -0300

    auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,22 @@
-module.exports = (req, res) => {
-  const authHead = req.headers.authorization
+const jwt = require('jsonwebtoken')
+const ENV = require('../config/env.config')
+const { promisify } = require('util')
 
-  if (!authHead) {
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization
 
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, ENV.SECRET)
+    req.userId = decoded.id
+
+    return next()
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid token' })
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ const express = require('express')
 const routes = express.Router()
 const UserController = require('./controller/UserController')
 const SessionController = require('./controller/SessionController')
+const authMiddleware = require('./middleware/auth')
 
 routes.post('/api/users', UserController.store)
 routes.post('/api/users/login', SessionController.store)
+
+routes.get('/api/auth', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
