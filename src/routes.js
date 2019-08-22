d520db6d0ec3f658294fa7ef6e46e8223cfc5c5c
georgemmp <machadogeorge.p@gmail.com>
2019-08-22 11:08:21 -0300

    validação de campos

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 const UserController = require('./controller/UserController')
@@ -6,18 +7,19 @@ const SessionController = require('./controller/SessionController')
 const authMiddleware = require('./middleware/auth')
 const AdController = require('./controller/AdController')
 const Purchase = require('./controller/PurchaseController')
+const validators = require('./validators')
 
-routes.post('/api/users', UserController.store)
-routes.post('/api/users/login', SessionController.store)
+routes.post('/api/users', validate(validators.UserValidator), UserController.store)
+routes.post('/api/users/login', validate(validators.SessionValidator), SessionController.store)
 
 routes.use(authMiddleware)
 
 routes.get('/api/ads', AdController.index)
 routes.get('/api/ads/:id', AdController.show)
-routes.post('/api/ads', AdController.store)
-routes.put('/api/ads/:id', AdController.update)
+routes.post('/api/ads', validate(validators.AdValidator), AdController.store)
+routes.put('/api/ads/:id', validate(validators.AdValidator), AdController.update)
 routes.delete('/api/ads/:id', AdController.destroy)
 
-routes.put('/api/purchase', Purchase.store)
+routes.put('/api/purchase', validate(validators.PurchaseValidator), Purchase.store)
 
 module.exports = routes
