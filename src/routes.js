0c50a0217a0946fd68e472ab1ad8a91ecdede01a
georgemmp <machadogeorge.p@gmail.com>
2019-08-22 11:31:21 -0300

    Tratamento de erros na aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const validate = require('express-validation')
+const handle = require('express-async-handler')
 
 const routes = express.Router()
 const UserController = require('./controller/UserController')
@@ -9,17 +10,17 @@ const AdController = require('./controller/AdController')
 const Purchase = require('./controller/PurchaseController')
 const validators = require('./validators')
 
-routes.post('/api/users', validate(validators.UserValidator), UserController.store)
-routes.post('/api/users/login', validate(validators.SessionValidator), SessionController.store)
+routes.post('/api/users', validate(validators.UserValidator), handle(UserController.store))
+routes.post('/api/users/login', validate(validators.SessionValidator), handle(SessionController.store))
 
 routes.use(authMiddleware)
 
-routes.get('/api/ads', AdController.index)
-routes.get('/api/ads/:id', AdController.show)
-routes.post('/api/ads', validate(validators.AdValidator), AdController.store)
-routes.put('/api/ads/:id', validate(validators.AdValidator), AdController.update)
-routes.delete('/api/ads/:id', AdController.destroy)
+routes.get('/api/ads', handle(AdController.index))
+routes.get('/api/ads/:id', handle(AdController.show))
+routes.post('/api/ads', validate(validators.AdValidator), handle(AdController.store))
+routes.put('/api/ads/:id', validate(validators.AdValidator), handle(AdController.update))
+routes.delete('/api/ads/:id', handle(AdController.destroy))
 
-routes.put('/api/purchase', validate(validators.PurchaseValidator), Purchase.store)
+routes.put('/api/purchase', validate(validators.PurchaseValidator), handle(Purchase.store))
 
 module.exports = routes
